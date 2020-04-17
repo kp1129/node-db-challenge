9d5c9703de31d0ba9de7051670152ad3d5aabf7d
kp1129 <katya.p.1129@gmail.com>
2020-04-17 09:59:02 -0700

    add post and get api request handlers + data access layer for /api/tasks

--- tasksRouter.js
+++ tasksRouter.js
@@ -1,18 +1,26 @@
 const express = require('express');
+const Tasks = require('./tasksModel');
 const router = express.Router();
 
 // API endpoints to
 // add tasks (must specify the project_id of an existing project)
+router.post('/', (req, res) => {
+    const newTask = req.body;
+    if(!newTask.description || !newTask.project_id){
+        res.status(400).json({ errorMessage: "please provide task description and project_id" })
+    } else {
+        Tasks.add(newTask)
+        .then(response => res.status(201).json({ message: "task successfully created" }))
+        .catch(error => res.status(500).json({ errorMessage: "could not create task at this time" }))
+    }
+})
 
 
 // retrieve a list of tasks (the list of tasks should include the project name and project description)
-
-
-
-
-// TEST
 router.get('/', (req, res) => {
-    res.status(200).json({ message: "hello from tasks!"})
-});
+    Tasks.get()
+    .then(response => res.status(200).json(response))
+    .catch(error => res.status(500).json({ errorMessage: "could not retrieve tasks at this time" }))
+})
 
 module.exports = router;
